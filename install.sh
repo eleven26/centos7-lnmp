a5077b381392f2ddeb030d4d77740ace1a2dc863
baiguiren <baiguiren@baiguiren-mac.local>
2016-09-28 13:35:30 +0800

    syntactic optimize

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 ########################################################################################################################
 # base dependency package
 yum -y install net-tools wget vim gcc git autoconf bzip2
@@ -14,7 +16,7 @@ yum -y install pcre-devel zlib-devel
 yum -y install libxml2-devel openssl-devel curl-devel libjpeg-devel libpng-devel freetype-devel openldap-devel
 
 
-centos_version=`hostnamectl | grep "Operating System" | awk '{print $5}'`
+centos_version=$(hostnamectl | grep "Operating System" | awk '{print $5}')
 if [[ ${centos_version} -ne 7 ]]
 then
     yum -y install libmcrypt-devel
@@ -61,8 +63,7 @@ nginx_pkg_name=$(echo ${nginx_pkg_url} | awk -F '/' '{print $NF}')
 
 # add some environment variable
 function add_env() {
-    path=$1
-    printf "\nexport PATH=$1:\$PATH\n" >> ${profile_file}
+    printf "\nexport PATH=%s:\$PATH\n" "$1" >> ${profile_file}
     return 0
 }
 
@@ -84,16 +85,16 @@ function get_unpacked_name(){
 }
 
 # get unpacked directory name not the full path, the installation will need these
-mysql_directory=`get_unpacked_name ${mysql_pkg_name}`
-php_directory=`get_unpacked_name ${php_pkg_name}`
-redis_directory=`get_unpacked_name ${redis_pkg_name}`
-nginx_directory=`get_unpacked_name ${nginx_pkg_name}`
+mysql_directory=$(get_unpacked_name "${mysql_pkg_name}")
+php_directory=$(get_unpacked_name "${php_pkg_name}")
+redis_directory=$(get_unpacked_name "${redis_pkg_name}")
+nginx_directory=$(get_unpacked_name "${nginx_pkg_name}")
 
 if [[ ! -d ${save_path} ]]
 then
-    printf "The directory ${save_path} doesn't exist, now create it.\n"
+    printf "The directory %s doesn't exist, now create it.\n" "${save_path}"
     mkdir ${save_path}
-    printf "${save_path} created.\n"
+    printf "%s created.\n" "${save_path}"
 else
     printf "Begin to check installation packages if exist...\n";
     printf "If packages doesn't exist, it will be download from the internet.\n"
@@ -102,28 +103,28 @@ fi
 # mysql
 if [[ ! -f ${mysql_path} ]]
 then
-    printf "${mysql_path} doesn't exist, begin to download....\n"
+    printf "%s doesn't exist, begin to download....\n" "${mysql_path}"
     wget -O ${mysql_path} ${mysql_pkg_url}
 fi
 
 # php
 if [[ ! -f ${php_path} ]]
 then
-    printf "${php_path} doesn't exist, begin to download...\n"
+    printf "%s doesn't exist, begin to download...\n" "${php_path}"
     wget -O ${php_path} ${php_pkg_url}
 fi
 
 # nginx
 if [[ ! -f ${nginx_path} ]]
 then
-    printf "${nginx_path} doesn't exist, begin to download...\n"
+    printf "%s doesn't exist, begin to download...\n" "${nginx_path}"
     wget -O ${nginx_path} ${nginx_pkg_url}
 fi
 
 # redis
 if [[ ! -f ${redis_path} ]]
 then
-    printf "${redis_path} doesn't exist, begin to download...\n"
+    printf "%s doesn't exist, begin to download...\n" "${redis_path}"
     wget -O ${redis_path} ${redis_pkg_url}
 fi
 
@@ -131,28 +132,28 @@ fi
 # mysql
 if [[ ! -f ${mysql_path} ]]
 then
-    printf "Download ${mysql_pkg_name} failed! please check if the given url is valid or check if the save path is valid."
+    printf "Download %s failed! please check if the given url is valid or check if the save path is valid." "${mysql_pkg_name}"
     exit 1
 fi
 
 # php
 if [[ ! -f ${php_path} ]]
 then
-    printf "Download ${php_pkg_name} failed! please check if the given url is valid or check if the save path is valid."
+    printf "Download %s failed! please check if the given url is valid or check if the save path is valid." "${php_pkg_name}"
     exit 1
 fi
 
 # nginx
 if [[ ! -f ${nginx_path} ]]
 then
-    printf "Download ${nginx_pkg_name} failed! please check if the given url is valid or check if the save path is valid."
+    printf "Download %s failed! please check if the given url is valid or check if the save path is valid." "${nginx_pkg_name}"
     exit 1
 fi
 
 # redis
 if [[ ! -f ${redis_path} ]]
 then
-    printf "Download ${redis_pkg_name} failed! please check if the given url is valid or check if the save path is valid."
+    printf "Download %s failed! please check if the given url is valid or check if the save path is valid." "${redis_pkg_name}"
     exit 1
 fi
 
@@ -160,23 +161,23 @@ fi
 # install mysql
 # cd ${save_path}
 # use the absolute path instead of enter the save path
-printf "Decompressing ${mysql_path} to ${save_path}....\n"
+printf "Decompressing %s to %s....\n" "${mysql_path}" "${save_path}"
 tar -xvf ${mysql_path} -C ${save_path}
 
-printf "Decompressing ${save_path}/${mysql_pkg_name}.gz to /usr/local\n"
+printf "Decompressing %s/%s.gz to /usr/local\n" "${save_path}" "${mysql_pkg_name}"
 tar -xvf "${save_path}/${mysql_pkg_name}.gz" -C /usr/local
 
 printf "Creating mysql soft link.\n"
 ln -s "/usr/local/${mysql_directory}" /usr/local/mysql
 
 printf "Adding mysql lib to /etc/ld.so.conf.\n"
-cat /etc/ld.so.conf | grep "/usr/local/mysql/lib" || printf "\n/usr/local/mysql/lib\n" >> /etc/ld.so.conf
+grep "/usr/local/mysql/lib" /etc/ld.so.conf || printf "\n/usr/local/mysql/lib\n" >> /etc/ld.so.conf
 ldconfig -v
 
 if [[ ! -d /usr/local/${mysql_directory} ]]
 then
-    printf "/usr/local/${mysql_directory} doesn't exist.\n"
-    printf "Decompression ${mysql_pkg_name} failed! Installation was interrupted.\n"
+    printf "/usr/local/%s doesn't exist.\n" "${mysql_directory}"
+    printf "Decompression %s failed! Installation was interrupted.\n" "${mysql_pkg_name}"
     exit 2
 fi
 
@@ -194,7 +195,7 @@ fi
 
 if [[ -f ${shell_script_path}/mysql/my.cnf ]]; then
     printf "Copy mysql configuration file to /etc...\n"
-    cp ${shell_script_path}/mysql/my.cnf /etc
+    cp "${shell_script_path}/mysql/my.cnf" /etc
 else
     message="${shell_script_path}/mysql/mysql.cnf doesn't exist!\n"
     printf message
@@ -202,8 +203,8 @@ else
 fi
 
 if [[ -f ${shell_script_path}/systemd/mysql.service ]]; then
-    printf "Copy mysql service file to ${service_path}, so that mysql can run as a system's service...\n"
-    cp ${shell_script_path}/systemd/mysql.service ${service_path}
+    printf "Copy mysql service file to %s, so that mysql can run as a system's service...\n" "${service_path}"
+    cp "${shell_script_path}/systemd/mysql.service" ${service_path}
 else
     message="${shell_script_path}/systemd/mysql.service doesn't exist!\n"
     printf message
@@ -227,6 +228,7 @@ printf "Begin to initializing mysql....\n"
 # add mysql bin to environment
 printf "\n"
 add_env /usr/local/mysql/bin
+# shellcheck source=/dev/null
 source ${profile_file}
 
 if [[ -f ${shell_script_path}/systemd/mysql.service ]]; then
@@ -251,7 +253,8 @@ tar -xvf ${nginx_path} -C ${save_path}
 # we cannot use absolute path because nginx's configure file contains relative path
 nginx_work_directory=${save_path}/${nginx_directory}
 
-cd ${nginx_work_directory}
+printf "Exit script if %s doesn't exist." "${nginx_work_directory}"
+cd "${nginx_work_directory}" || exit 1
 ./configure --with-http_stub_status_module
 make
 make install
@@ -260,6 +263,7 @@ make install
 ln -s /usr/local/nginx/sbin/nginx /usr/local/bin/
 if [[ ! -s /usr/local/bin/nginx ]]; then
     add_env /usr/local/nginx/bin
+    # shellcheck source=/dev/null
     source ${profile_file}
 fi
 
@@ -270,16 +274,16 @@ then
     mkdir ${vhost_dir}
 fi
 # add vhost example
-cp ${shell_script_path}/nginx/vhost/vhost-default.conf.example /usr/local/nginx/vhost
+cp "${shell_script_path}/nginx/vhost/vhost-default.conf.example" /usr/local/nginx/vhost
 rewrite_dir=/usr/local/nginx/conf/rewrite
 if [[ ! -d ${rewrite_dir} ]]
 then
     mkdir ${rewrite_dir}
 fi
-cp ${shell_script_path}/nginx/rewrite/laravel.conf /usr/local/nginx/rewrite
+cp "${shell_script_path}/nginx/rewrite/laravel.conf" /usr/local/nginx/rewrite
 
 # add nginx service
-cp ${shell_script_path}/systemd/nginx.service ${service_path}
+cp "${shell_script_path}/systemd/nginx.service" ${service_path}
 if [[ -f ${shell_script_path}/systemd/nginx.service ]]; then
     systemctl enable nginx
     systemctl start nginx
@@ -289,7 +293,8 @@ else
     printf message >> ${install_log}
 fi
 
-cd ${shell_script_path}
+printf "Exit the script if %s doesn't exist.\n" "${shell_script_path}"
+cd "${shell_script_path}" || exit 1
 ########################################################################################################################
 
 
@@ -300,13 +305,14 @@ tar -xvf ${php_path} -C ${save_path}
 libxml2_lib=/usr/include/libxml2/libxml
 if [[ -d ${libxml2_lib} ]]
 then
-    cat /etc/ld.so.conf | grep "${libxml2_lib}" || printf "\n${libxml2_lib}\n" >> /etc/ld.so.conf
+    grep "${libxml2_lib}" /etc/ld.so.conf || printf "\n%s\n" "${libxml2_lib}" >> /etc/ld.so.conf
 fi
 ldconfig -v
 
 php_work_directory=${save_path}/${php_directory}
 # configure
-cd ${php_work_directory}
+printf "Exit the script if directory %s doesn't exist.\n" "${php_work_directory}"
+cd "${php_work_directory}" || exit 4
 ./configure --prefix=/usr/local/php --with-config-file-path=/usr/local/php/etc --with-mysql=/usr/local/mysql --with-mysqli=/usr/local/mysql/bin/mysql_config --with-iconv --with-freetype-dir --with-jpeg-dir --with-png-dir --with-zlib --with-libxml-dir=/usr --enable-xml --disable-rpath --enable-discard-path --enable-safe-mode --enable-bcmath --enable-shmop --enable-sysvsem --enable-inline-optimization --with-curl --with-curlwrappers --enable-mbregex --enable-fastcgi --enable-fpm --enable-force-cgi-redirect --enable-mbstring --with-mcrypt --with-gd --enable-gd-native-ttf --with-openssl --with-mhash --enable-pcntl --enable-sockets --with-xmlrpc --enable-zip --enable-soap --without-pear --with-zlib --enable-pdo --with-pdo-mysql --enable-opcache
 make
 make install
@@ -323,6 +329,7 @@ sed -i "s/upload_max_filesize = 2M/upload_max_filesize = 50M/g" /usr/local/php/e
 # add environment variable
 printf "Add php bin to environment varionment."
 add_env /usr/local/php/bin:/usr/local/php/sbin
+# shellcheck source=/dev/null
 source ${profile_file}
 
 # add user www and group www
@@ -342,7 +349,7 @@ fi
 
 if [[ ! -f /usr/local/php/etc/php-fpm.d/www.conf ]]; then
     message="/usr/local/php/etc/php-fpm.d/www.conf doesn't exist! Please configure the php-fpm manually!"
-    printf ${message}
+    printf "%s" "${message}"
 else
     printf "Now modify www.conf to change user and group from nobody to www...\n"
     sed -i "s/user = nobody/user = www/g" /usr/local/php/etc/php-fpm.d/www.conf
@@ -350,14 +357,16 @@ else
 fi
 
 # add php-fpm service
-cp ${shell_script_path}/systemd/php-fpm.service ${service_path}
+cp "${shell_script_path}/systemd/php-fpm.service" ${service_path}
 if [[ ! -f ${shell_script_path}/systemd/php-fpm.service ]]; then
-    printf "${shell_script_path}/systemd/php-fpm.service doesn't exist.\n We can not start php-fpm service!\n"
+    printf "%s/systemd/php-fpm.service doesn't exist.\n We can not start php-fpm service!\n" "${shell_script_path}"
 else
     systemctl enable php-fpm
     systemctl start php-fpm
 fi
-cd ${shell_script_path}
+
+printf "Exit the script if %s doesn't exist.\n" "${shell_script_path}"
+cd "${shell_script_path}" || exit 1
 ########################################################################################################################
 
 
@@ -366,12 +375,14 @@ cd ${shell_script_path}
 # cd ${save_path}
 tar -xvf ${redis_path} -C ${save_path}
 redis_work_directory=${save_path}/${redis_directory}
-cd ${redis_work_directory}
+
+printf "Exit the script if directory %s doesn't exist.\n" "${redis_work_directory}"
+cd "${redis_work_directory}" || exit 1
 make
 make install
 
 if [[ -f ${redis_work_directory}/redis.conf ]]; then
-    cp ${redis_work_directory}/redis.conf /etc
+    cp "${redis_work_directory}/redis.conf" /etc
 else
     printf "Redis configuration file doesn't exist. please add it manually!\n"
 fi
@@ -383,16 +394,19 @@ else
 fi
 
 # add redis service
-cp ${shell_script_path}/systemd/redis.service ${service_path}
+cp "${shell_script_path}/systemd/redis.service" ${service_path}
 if [[ -f ${service_path}/redis.service ]]; then
     systemctl enable redis
     systemctl start redis
 fi
 
 # install php redis extension
-cd ${shell_script_path}
+printf "Exit the script if directory %s doesn't exist.\n" "${shell_script_path}"
+cd "${shell_script_path}" || exit 1
 git clone https://github.com/phpredis/phpredis.git
-cd phpredis
+#todo download before installation
+printf "Exit the script if phpredis can not downloaded succeefully.\n"
+cd phpredis || exit 1
 git checkout -b php7 origin/php7
 phpize
 ./configure --with-php-config=php-config
@@ -405,6 +419,7 @@ if [[ -f /usr/local/php/etc/php.ini ]]; then
 fi
 
 systemctl restart php-fpm
+# shellcheck source=/dev/null
 source ${profile_file}
-cd ${shell_script_path}
+cd "${shell_script_path}" || exit 1
 ########################################################################################################################

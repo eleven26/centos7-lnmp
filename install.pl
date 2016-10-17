2d494361594e167110b6d62540827098296f0417
baiguiren <baiguiren@kollway.com>
2016-10-17 14:02:43 +0800

    0==system ...

--- install.pl
+++ install.pl
@@ -176,7 +176,7 @@ unless (-d $install_dir.'/mysql')
     system "mkdir $install_dir/mysql/data" unless -d $install_dir.'/mysql/data';
 
     say "Copy mysql configuration file ${current_path}/mysql/my.cnf to /etc.";
-    system "cp ${current_path}/mysql/my.cnf /etc" == 0 or warn "can not copy mysql configuration file: $!.";
+    0 == system "cp ${current_path}/mysql/my.cnf /etc" or warn "can not copy mysql configuration file: $!.";
 
     say "Copy mysql service file ${current_path}/systemd/mysql.service to $service_path, so that mysql can run as a system's service.";
     system "cp ${current_path}/systemd/mysql.service ${service_path}";
@@ -185,10 +185,10 @@ unless (-d $install_dir.'/mysql')
     say "Now initializing mysql, after this finishes, it will generate the initializing password for root";
     # add user and group
     say "Adding user mysql and group mysql.";
-    system "groupadd mysql" == 0 or warn "can not add mysql group: $!";
-    system "useradd -r -g mysql -s /bin/false mysql" == 0 or warn "can not add use mysql: $!";
+    0 == system "groupadd mysql" or warn "can not add mysql group: $!";
+    0 == system "useradd -r -g mysql -s /bin/false mysql" or warn "can not add use mysql: $!";
     say "Changing mysql directory owned by mysql.";
-    system "chown -R mysql:mysql $install_dir/mysql" == 0 or warn "$install_dir/mysql can not be chown: $!";
+    0 == system "chown -R mysql:mysql $install_dir/mysql" or warn "$install_dir/mysql can not be chown: $!";
 
     # attention: the next line will generate output in ~/mysql_initialize, the root's password will be appeared in that file
     say "Begin to initializing mysql.";

7768869d640fb9bdfcaf9ce51c2db28e9f7bca46
ruby <916809498@qq.com>
2016-12-12 22:59:50 +0800

    add clean.sh to clean after failed installing package

---
+++ clean.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+#set -x
+
+source config.sh
+
+function usage() { 
+    printf "usage: ./clean.sh [--deps] [--depends] [--dependency]\n"
+    printf "                  [--redis]\n"
+    printf "                  [--mysql]\n"
+    printf "                  [--php]\n"
+    printf "                  [--nginx]\n"
+    printf "     --deps | --depends | --dependency     remove installed dependency\n"
+    printf "     --redis                                 remove redis\n"
+    printf "     --mysql                                 remove mysql\n"
+    printf "     --php                                   remove php\n"
+    printf "     --nginx                                 remove nginx\n"
+    printf "     --all                                   remove everything downloaded and installed\n"
+    exit 1
+}
+
+if [ "$1" == "" ]; then
+    usage
+fi
+
+while [ "$1" != "" ]; do
+    case $1 in
+        --deps | --depends | --dependency )
+            # delete installed dependency
+            yum remove net-tools wget vim gcc git autoconf bzip2 libaio pcre-devel make \
+                zlib-devel libxml2-devel openssl-devel curl-devel libjpeg-devel libpng-devel freetype-devel openldap-devel \
+                libmcrypt-devel epel-release libmcrypt-devel.x86_64
+            ;;
+        --redis )
+            rm -rf ${save_path}/redis*
+            rm -rf /usr/local/redis*
+            rm -f ${service_path}/redis.service
+            ;;
+        --mysql )
+            rm -rf ${save_path}/mysql*
+            rm -rf /usr/local/mysql*
+            rm -f /etc/my.cnf
+            rm -f ${service_path}/mysql.service
+            ;;
+        --php )
+            rm -rf ${save_path}/php*
+            rm -rf /usr/local/php*
+            ;;
+        --nginx )
+            rm -rf ${save_path}/nginx*
+            rm -rf /usr/local/nginx*
+            rm -f ${service_path}/nginx.service
+            ;;
+        --all )
+            # remove everything
+            set -- "--all" "--deps" "--redis" "--mysqsl" "--php" "--nginx"
+            ;;
+            * )
+                usage
+                exit
+    esac
+    shift
+done
---
+++ config.sh
@@ -0,0 +1,91 @@
+current_path=$(pwd)
+profile_file=/etc/profile
+service_path=/lib/systemd/system
+
+save_path=${current_path}/downloads
+
+# packages download's url
+mysql_pkg_url=http://dev.mysql.com/get/Downloads/MySQL-5.7/mysql-5.7.15-linux-glibc2.5-x86_64.tar
+php_pkg_url=http://cn2.php.net/get/php-7.0.11.tar.bz2/from/this/mirror
+redis_pkg_url=http://download.redis.io/redis-stable.tar.gz
+nginx_pkg_url=http://nginx.org/download/nginx-1.11.4.tar.gz
+phpredis_pkg_url=https://github.com/phpredis/phpredis.git
+
+# get file name with file extension
+mysql_pkg_name=$(echo ${mysql_pkg_url} | awk -F '/' '{print $NF}')
+php_pkg_name=$(echo ${php_pkg_url} | awk -F '/' '{print $5}')
+redis_pkg_name=$(echo ${redis_pkg_url} | awk -F '/' '{print $NF}')
+nginx_pkg_name=$(echo ${nginx_pkg_url} | awk -F '/' '{print $NF}')
+
+php_configure_option=$(cat << EOF
+--prefix=/usr/local/php \
+--with-config-file-path=/usr/local/php/etc \
+--with-mysqli=/usr/local/mysql/bin/mysql_config \
+--with-iconv \
+--with-freetype-dir \
+--with-jpeg-dir \
+--with-png-dir \
+--with-zlib \
+--with-libxml-dir \
+--enable-xml \
+--disable-rpath \
+--enable-safe-mode \
+--enable-shmop \
+--enable-sysvsem \
+--enable-inline-optimization \
+--with-curl \
+--enable-mbregex \
+--enable-fpm \
+--enable-mbstring \
+--with-mcrypt \
+--with-gd \
+--enable-gd-native-ttf \
+--with-openssl \
+--with-mhash \
+--enable-pcntl \
+--enable-sockets \
+--with-xmlrpc \
+--enable-zip \
+--enable-soap \
+--without-pear \
+--with-zlib \
+--enable-pdo \
+--with-pdo-mysql \
+--enable-opcache
+EOF
+)
+
+# add some environment variable
+function add_env() {
+    printf "\nexport PATH=%s:\$PATH\n" "$1" >> ${profile_file}
+    return 0
+}
+
+# get the correct directory name after unpacked
+function get_unpacked_name(){
+    local filename=$1
+    local extension="${filename##*.}"
+    while true
+    do
+        if [[ ${extension} != "tar" && ${extension} != "gz" && ${extension} != "bz2" && ${extension} != "xz" ]]
+        then
+            echo "${filename}"
+            return 0
+        else
+            filename="${filename%.*}"
+            extension="${filename##*.}"
+        fi
+    done
+}
+
+# get unpacked directory name not the full path, the installation will need these
+mysql_directory=$(get_unpacked_name "${mysql_pkg_name}")
+php_directory=$(get_unpacked_name "${php_pkg_name}")
+redis_directory=$(get_unpacked_name "${redis_pkg_name}")
+nginx_directory=$(get_unpacked_name "${nginx_pkg_name}")
+phpredis_directory=${save_path}/phpredis
+
+mysql_install_dir=/usr/local/mysql
+php_install_dir=/usr/local/php
+redis_install_dir=/usr/local/redis
+nginx_install_dir=/usr/local/nginx

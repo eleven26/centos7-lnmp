ba06a56ebae9007e41c8508f46b882c70c8be38f
ruby <916809498@qq.com>
2016-12-12 21:01:02 +0800

    cannot use --enable-mcrypt

--- install.sh
+++ install.sh
@@ -22,15 +22,15 @@ yum -y install net-tools wget vim gcc git autoconf bzip2 libaio pcre-devel make 
     zlib-devel libxml2-devel openssl-devel curl-devel libjpeg-devel libpng-devel freetype-devel openldap-devel
 
 centos_version=$(hostnamectl | grep "Operating System" | awk '{print $5}')
-#if [[ ${centos_version} -ne 7 ]]
-#then
-#    yum -y install libmcrypt-devel
-#else
+if [[ ${centos_version} -ne 7 ]]
+then
+    yum -y install libmcrypt-devel
+else
     # yum -y install libmcrypt-devel # doesn't work in centos7
     # centos7 php openssl dependency package
-#    yum -y install epel-release
-#    yum -y install libmcrypt-devel.x86_64
-#fi
+    yum -y install epel-release
+    yum -y install libmcrypt-devel.x86_64
+fi
 ########################################################################################################################
 
 # NLS nuisances.
